03e53d78698f4d4bc1c5c68cb97d878070373a7a
Stuart Schechter <stuart.schechter@gmail.com>
2020-04-02 13:21:35 +0900

    remove superfluous .h files from cmake

--- extern/libsodium.cmake
+++ extern/libsodium.cmake
@@ -31,13 +31,6 @@ option(SODIUM_ENABLE_BLOCKING_RANDOM "Enable this switch only if /dev/urandom is
 option(SODIUM_PRETEND_TO_BE_CONFIGURED "Silence warnings about build system not being properly configured" OFF)
 
 set (SODIUM_SRC_BASE ${LIBSODIUM_REPOSITORY_DIR}/src/libsodium)
-file(GLOB SRCS
-    "*.h"
-    "*/*.h"
-    "*.cpp"
-    "*/*.cpp"
-)
-
 
 add_library(${PROJECT_NAME}
     ${SODIUM_SRC_BASE}/crypto_aead/aes256gcm/aesni/aead_aes256gcm_aesni.c
@@ -52,30 +45,15 @@ add_library(${PROJECT_NAME}
     ${SODIUM_SRC_BASE}/crypto_box/crypto_box_seal.c
     ${SODIUM_SRC_BASE}/crypto_box/curve25519xsalsa20poly1305/box_curve25519xsalsa20poly1305.c
     ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/ed25519_ref10.c
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_25_5/base.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_25_5/base2.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_25_5/constants.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_25_5/fe.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_51/base.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_51/base2.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_51/constants.h
-    ${SODIUM_SRC_BASE}/crypto_core/ed25519/ref10/fe_51/fe.h
     ${SODIUM_SRC_BASE}/crypto_core/hchacha20/core_hchacha20.c
     ${SODIUM_SRC_BASE}/crypto_core/hsalsa20/core_hsalsa20.c
     ${SODIUM_SRC_BASE}/crypto_core/hsalsa20/ref2/core_hsalsa20_ref2.c
     ${SODIUM_SRC_BASE}/crypto_core/salsa/ref/core_salsa_ref.c
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/generichash_blake2.c
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2.h
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-avx2.c
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-avx2.h
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-ref.c
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-sse41.c
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-sse41.h
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-ssse3.c
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-compress-ssse3.h
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-load-avx2.h
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-load-sse2.h
-    ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-load-sse41.h
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/blake2b-ref.c
     ${SODIUM_SRC_BASE}/crypto_generichash/blake2b/ref/generichash_blake2b.c
     ${SODIUM_SRC_BASE}/crypto_generichash/crypto_generichash.c
@@ -89,163 +67,49 @@ add_library(${PROJECT_NAME}
     ${SODIUM_SRC_BASE}/crypto_kx/crypto_kx.c
     ${SODIUM_SRC_BASE}/crypto_onetimeauth/crypto_onetimeauth.c
     ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/donna/poly1305_donna.c
-    ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/donna/poly1305_donna.h
-    ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/donna/poly1305_donna32.h
-    ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/donna/poly1305_donna64.h
     ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/onetimeauth_poly1305.c
-    ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/onetimeauth_poly1305.h
     ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/sse2/poly1305_sse2.c
-    ${SODIUM_SRC_BASE}/crypto_onetimeauth/poly1305/sse2/poly1305_sse2.h
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-core.c
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-core.h
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-encoding.c
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-encoding.h
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-fill-block-avx2.c
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-fill-block-avx512f.c
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-fill-block-ref.c
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2-fill-block-ssse3.c
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2.c
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/argon2.h
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/blake2b-long.c
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/blake2b-long.h
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/blamka-round-avx2.h
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/blamka-round-avx512f.h
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/blamka-round-ref.h
-    ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/blamka-round-ssse3.h
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/pwhash_argon2i.c
     ${SODIUM_SRC_BASE}/crypto_pwhash/argon2/pwhash_argon2id.c
     ${SODIUM_SRC_BASE}/crypto_pwhash/crypto_pwhash.c
     ${SODIUM_SRC_BASE}/crypto_scalarmult/crypto_scalarmult.c
     ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/ref10/x25519_ref10.c
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/ref10/x25519_ref10.h
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/consts_namespace.h
     ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/curve25519_sandy2x.c
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/curve25519_sandy2x.h
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/fe.h
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/fe51.h
     ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/fe51_invert.c
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/fe51_namespace.h
     ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/fe_frombytes_sandy2x.c
-#    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/ladder.h
-#    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/ladder_base.h
-#    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/ladder_base_namespace.h
-#    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/sandy2x/ladder_namespace.h
     ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/scalarmult_curve25519.c
-    ${SODIUM_SRC_BASE}/crypto_scalarmult/curve25519/scalarmult_curve25519.h
     ${SODIUM_SRC_BASE}/crypto_secretbox/crypto_secretbox.c
     ${SODIUM_SRC_BASE}/crypto_secretbox/crypto_secretbox_easy.c
     ${SODIUM_SRC_BASE}/crypto_secretbox/xsalsa20poly1305/secretbox_xsalsa20poly1305.c
     ${SODIUM_SRC_BASE}/crypto_secretstream/xchacha20poly1305/secretstream_xchacha20poly1305.c
     ${SODIUM_SRC_BASE}/crypto_shorthash/crypto_shorthash.c
     ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/ref/shorthash_siphash24_ref.c
-    ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/ref/shorthash_siphash_ref.h
     ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/shorthash_siphash24.c
     ${SODIUM_SRC_BASE}/crypto_sign/crypto_sign.c
     ${SODIUM_SRC_BASE}/crypto_sign/ed25519/ref10/keypair.c
     ${SODIUM_SRC_BASE}/crypto_sign/ed25519/ref10/open.c
     ${SODIUM_SRC_BASE}/crypto_sign/ed25519/ref10/sign.c
-    ${SODIUM_SRC_BASE}/crypto_sign/ed25519/ref10/sign_ed25519_ref10.h
     ${SODIUM_SRC_BASE}/crypto_sign/ed25519/sign_ed25519.c
     ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/chacha20_dolbeau-avx2.c
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/chacha20_dolbeau-avx2.h
     ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/chacha20_dolbeau-ssse3.c
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/chacha20_dolbeau-ssse3.h
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/u0.h
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/u1.h
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/u4.h
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/dolbeau/u8.h
     ${SODIUM_SRC_BASE}/crypto_stream/chacha20/ref/chacha20_ref.c
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/ref/chacha20_ref.h
     ${SODIUM_SRC_BASE}/crypto_stream/chacha20/stream_chacha20.c
-    ${SODIUM_SRC_BASE}/crypto_stream/chacha20/stream_chacha20.h
     ${SODIUM_SRC_BASE}/crypto_stream/crypto_stream.c
     ${SODIUM_SRC_BASE}/crypto_stream/salsa20/ref/salsa20_ref.c
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/ref/salsa20_ref.h
     ${SODIUM_SRC_BASE}/crypto_stream/salsa20/stream_salsa20.c
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/stream_salsa20.h
     ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6/salsa20_xmm6.c
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6/salsa20_xmm6.h
     ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/salsa20_xmm6int-avx2.c
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/salsa20_xmm6int-avx2.h
     ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/salsa20_xmm6int-sse2.c
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/salsa20_xmm6int-sse2.h
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/u0.h
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/u1.h
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/u4.h
-    ${SODIUM_SRC_BASE}/crypto_stream/salsa20/xmm6int/u8.h
     ${SODIUM_SRC_BASE}/crypto_stream/xsalsa20/stream_xsalsa20.c
     ${SODIUM_SRC_BASE}/crypto_verify/sodium/verify.c
-    ${SODIUM_SRC_BASE}/include/sodium.h
-    ${SODIUM_SRC_BASE}/include/sodium/core.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_aead_aes256gcm.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_aead_chacha20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_aead_xchacha20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_auth.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_auth_hmacsha256.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_auth_hmacsha512.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_auth_hmacsha512256.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_box.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_box_curve25519xchacha20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_box_curve25519xsalsa20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_ed25519.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_hchacha20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_hsalsa20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_ristretto255.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_salsa20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_salsa2012.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_core_salsa208.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_generichash.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_generichash_blake2b.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_hash.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_hash_sha256.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_hash_sha512.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_kdf.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_kdf_blake2b.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_kx.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_onetimeauth.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_onetimeauth_poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_pwhash.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_pwhash_argon2i.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_pwhash_argon2id.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_pwhash_scryptsalsa208sha256.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_scalarmult.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_scalarmult_curve25519.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_scalarmult_ed25519.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_scalarmult_ristretto255.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_secretbox.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_secretbox_xchacha20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_secretbox_xsalsa20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_secretstream_xchacha20poly1305.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_shorthash.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_shorthash_siphash24.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_sign.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_sign_ed25519.h
-#    ${SODIUM_SRC_BASE}/include/sodium/crypto_sign_edwards25519sha512batch.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream_chacha20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream_salsa20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream_salsa2012.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream_salsa208.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream_xchacha20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_stream_xsalsa20.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_verify_16.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_verify_32.h
-    ${SODIUM_SRC_BASE}/include/sodium/crypto_verify_64.h
-    ${SODIUM_SRC_BASE}/include/sodium/export.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/chacha20_ietf_ext.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/common.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/ed25519_ref10.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/ed25519_ref10_fe_25_5.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/ed25519_ref10_fe_51.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/implementations.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/mutex.h
-    ${SODIUM_SRC_BASE}/include/sodium/private/sse2_64_32.h
-    ${SODIUM_SRC_BASE}/include/sodium/randombytes.h
-    ${SODIUM_SRC_BASE}/include/sodium/randombytes_internal_random.h
-    ${SODIUM_SRC_BASE}/include/sodium/randombytes_sysrandom.h
-    ${SODIUM_SRC_BASE}/include/sodium/runtime.h
-    ${SODIUM_SRC_BASE}/include/sodium/utils.h
-    ${SODIUM_SRC_BASE}/include/sodium/version.h
     ${SODIUM_SRC_BASE}/randombytes/internal/randombytes_internal_random.c
     ${SODIUM_SRC_BASE}/randombytes/randombytes.c
     ${SODIUM_SRC_BASE}/randombytes/sysrandom/randombytes_sysrandom.c
@@ -254,36 +118,27 @@ add_library(${PROJECT_NAME}
     ${SODIUM_SRC_BASE}/sodium/runtime.c
     ${SODIUM_SRC_BASE}/sodium/utils.c
     ${SODIUM_SRC_BASE}/sodium/version.c
+    ${SODIUM_SRC_BASE}/crypto_box/curve25519xchacha20poly1305/box_curve25519xchacha20poly1305.c
+    ${SODIUM_SRC_BASE}/crypto_box/curve25519xchacha20poly1305/box_seal_curve25519xchacha20poly1305.c
+    ${SODIUM_SRC_BASE}/crypto_core/ed25519/core_ed25519.c
+    ${SODIUM_SRC_BASE}/crypto_core/ed25519/core_ristretto255.c
+    ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt-common.c
+    ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/nosse/pwhash_scryptsalsa208sha256_nosse.c
+    ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.c
+    ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/pwhash_scryptsalsa208sha256.c
+    ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/scrypt_platform.c
+    ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/sse/pwhash_scryptsalsa208sha256_sse.c
+    ${SODIUM_SRC_BASE}/crypto_scalarmult/ed25519/ref10/scalarmult_ed25519_ref10.c
+    ${SODIUM_SRC_BASE}/crypto_scalarmult/ristretto255/ref10/scalarmult_ristretto255_ref10.c
+    ${SODIUM_SRC_BASE}/crypto_secretbox/xchacha20poly1305/secretbox_xchacha20poly1305.c
+    ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/ref/shorthash_siphashx24_ref.c
+    ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/shorthash_siphashx24.c
+    ${SODIUM_SRC_BASE}/crypto_stream/salsa2012/ref/stream_salsa2012_ref.c
+    ${SODIUM_SRC_BASE}/crypto_stream/salsa2012/stream_salsa2012.c
+    ${SODIUM_SRC_BASE}/crypto_stream/salsa208/ref/stream_salsa208_ref.c
+    ${SODIUM_SRC_BASE}/crypto_stream/salsa208/stream_salsa208.c
+    ${SODIUM_SRC_BASE}/crypto_stream/xchacha20/stream_xchacha20.c
 )
-
-if(NOT SODIUM_MINIMAL)
-    target_sources(${PROJECT_NAME}
-        PRIVATE
-            ${SODIUM_SRC_BASE}/crypto_box/curve25519xchacha20poly1305/box_curve25519xchacha20poly1305.c
-            ${SODIUM_SRC_BASE}/crypto_box/curve25519xchacha20poly1305/box_seal_curve25519xchacha20poly1305.c
-            ${SODIUM_SRC_BASE}/crypto_core/ed25519/core_ed25519.c
-            ${SODIUM_SRC_BASE}/crypto_core/ed25519/core_ristretto255.c
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt-common.c
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/crypto_scrypt.h
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/nosse/pwhash_scryptsalsa208sha256_nosse.c
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.c
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/pbkdf2-sha256.h
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/pwhash_scryptsalsa208sha256.c
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/scrypt_platform.c
-            ${SODIUM_SRC_BASE}/crypto_pwhash/scryptsalsa208sha256/sse/pwhash_scryptsalsa208sha256_sse.c
-            ${SODIUM_SRC_BASE}/crypto_scalarmult/ed25519/ref10/scalarmult_ed25519_ref10.c
-            ${SODIUM_SRC_BASE}/crypto_scalarmult/ristretto255/ref10/scalarmult_ristretto255_ref10.c
-            ${SODIUM_SRC_BASE}/crypto_secretbox/xchacha20poly1305/secretbox_xchacha20poly1305.c
-            ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/ref/shorthash_siphashx24_ref.c
-            ${SODIUM_SRC_BASE}/crypto_shorthash/siphash24/shorthash_siphashx24.c
-#            ${SODIUM_SRC_BASE}/crypto_sign/ed25519/ref10/obsolete.c
-            ${SODIUM_SRC_BASE}/crypto_stream/salsa2012/ref/stream_salsa2012_ref.c
-            ${SODIUM_SRC_BASE}/crypto_stream/salsa2012/stream_salsa2012.c
-            ${SODIUM_SRC_BASE}/crypto_stream/salsa208/ref/stream_salsa208_ref.c
-            ${SODIUM_SRC_BASE}/crypto_stream/salsa208/stream_salsa208.c
-            ${SODIUM_SRC_BASE}/crypto_stream/xchacha20/stream_xchacha20.c
-    )
-endif()
 
 set_target_properties(${PROJECT_NAME}
     PROPERTIES
